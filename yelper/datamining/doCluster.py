03ab54409aa0a2ed81973cffef28bddc1f7510ed
Sequømics <sequomics@gmail.com>
2015-12-09 17:06:21 +0530

    Initial commit
    metadata of Yelper®.

--- yelper/datamining/doCluster.py
+++ yelper/datamining/doCluster.py
@@ -1,1 +1,12 @@
 #!/usr/bin/env python
+
+
+__authors__ 	= [
+    '"Prabhat Kumar" <prabhat.genome@gmail.com>',
+    '"Aiswarya Thomas" <aiswaryathomas15@gmail.com>',
+    '"Sequømics Corporation" <admin@sequomics.com>'
+    ]
+__company__ 	= 'Sequømics Corporation'
+__homepage__ 	= 'http://sequomics.com/'
+__account__ 	= 'SequomicsCorporation'
+__githubURL__  = 'https://github.com/SequomicsCorporation'
