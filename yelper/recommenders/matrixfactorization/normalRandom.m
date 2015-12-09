8a47e612e61c2ab36ce35ccbc8de1c859d61cdbb
Sequømics <sequomics@gmail.com>
2015-12-09 22:48:43 +0530

    Initial commit
    To work on Normal Random.

---
+++ yelper/recommenders/matrixfactorization/normalRandom.m
@@ -0,0 +1,9 @@
+% =================================================================================================
+% Yelper®
+% Version 1.0.0
+% Date: 09-12-2015
+% Architecture and Code Handcrafted by Prabhat Kumar, http://prabhatkumar.org/.
+% Contributor(s): Aiswarya Thomas.
+% LICENSE: Apache, Version 2.0 | https://github.com/SequomicsCorporation/Yelper/blob/master/LICENSE
+% Copyright © 2014 - 2015, Sequømics Corporation. All rights reserved.
+% =================================================================================================
