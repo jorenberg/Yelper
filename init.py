7da1394a817c81038f29af693676e448f4a454ae
Sequømics <sequomics@gmail.com>
2015-12-09 14:49:02 +0530

    Initial commit
    Authors added.

--- init.py
+++ init.py
@@ -1,1 +1,8 @@
 #!/usr/bin/env python
+
+
+__authors__ 	= [
+    '"Prabhat Kumar" <prabhat.genome@gmail.com>',
+    '"Aiswarya Thomas" <aiswaryathomas15@gmail.com>',
+    '"Sequømics Corporation" <admin@sequomics.com>'
+    ]
