66bfae11802b0cbbfec03a9356ef3b618a080acd
Aiswarya Thomas <aiswaryat@users.noreply.github.com>
2015-12-21 23:22:55 +0530

    Initial commit
    importing Natural Language Toolkit — NLTK 3.0
    Help: http://www.nltk.org/.

--- yelper/etl/stemmer.py
+++ yelper/etl/stemmer.py
@@ -29,3 +29,4 @@ __license__     = 'Apache License'
 # See the License for the specific language governing permissions and
 # limitations under the License.
 # ========================================================================
+import nltk.stem
