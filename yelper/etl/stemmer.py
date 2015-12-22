8767638ad2e047e8a14df66765f15b8334489f82
sequomics <sequomics@gmail.com>
2015-12-22 22:59:29 +0530

    Initial commit
    for Count Vectorizer.

--- yelper/etl/stemmer.py
+++ yelper/etl/stemmer.py
@@ -32,3 +32,11 @@ __license__     = 'Apache License'
 import nltk.stem
 from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.feature_extraction.text import TfidfVectorizer
+
+english_stemmer = nltk.stem.SnowballStemmer('english')
+
+# S1: for Count Vectorizer.
+class StemmedCountVectorizer(CountVectorizer):
+    def build_analyzer(self):
+        analyzer = super(StemmedCountVectorizer, self).build_analyzer()
+        return lambda doc: (english_stemmer.stem(w) for w in analyzer(doc))
