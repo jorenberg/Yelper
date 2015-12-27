3f134c6be9cc48425f89eb42063be8591173810a
Aiswarya Thomas <aiswaryat@users.noreply.github.com>
2015-12-27 14:10:44 +0530

    Initial commit
    similarity calculation defined.

--- yelper/etl/similarityCalculator.py
+++ yelper/etl/similarityCalculator.py
@@ -47,3 +47,22 @@ def euclidean(vector1, vector2):
 # manhattan distance.
 def manhattan(vector1, vector2):
     return 1. / (1 + spatial.distance.cityblock(vector1, vector2))
+
+# similarity calculation.
+def calculate_similarity(vector1, vector2, similarity_metric='euclidean'):
+    if similarity_metric == 'chebyshev':
+        return chebyshev(vector1, vector2)
+    if similarity_metric == 'cosine':
+        return cosine(vector1, vector2)
+    if similarity_metric == 'euclidean':
+        return euclidean(vector1, vector2)
+    if similarity_metric == 'manhattan':
+        return manhattan(vector1, vector2)
+    if similarity_metric == 'pearson':
+        similarity_value = numpy.corrcoef(vector1, vector2)[0, 1]
+        if similarity_value <= 0 or math.isnan(similarity_value):
+            return None
+        return similarity_value
+    
+    msg = 'Yelper®: - Unrecognized similarity metric \'' + similarity_metric + '\''
+    raise ValueError(msg)
