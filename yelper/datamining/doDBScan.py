1d8e842c9e328f662aa6f0881f1bb854cd5ed08a
Aiswarya Thomas <aiswaryat@users.noreply.github.com>
2016-01-12 23:18:52 +0530

    Initial commit
    About and Reference of DBSCAN clustering algorithm.

--- yelper/datamining/doDBScan.py
+++ yelper/datamining/doDBScan.py
@@ -1,2 +1,17 @@
 #!/usr/bin/env python
 #  -*- coding: utf-8 -*-
+
+"""
+============================
+DBSCAN clustering algorithm
+============================
+Finds core samples of high density and expands clusters from them.
+------------------------------------------------------------------
+DBSCAN - Density-Based Spatial Clustering of Applications with Noise.
+Finds core samples of high density and expands clusters from them.
+Good for data which contains clusters of similar density.
+
+References:
+    Ester, M., H. P. Kriegel, J. Sander, and X. Xu, “A Density-Based Algorithm for Discovering Clusters in Large Spatial Databases with Noise”.
+    In: Proceedings of the 2nd International Conference on Knowledge Discovery and Data Mining, Portland, OR, AAAI Press, pp. 226-231. 1996.
+"""
